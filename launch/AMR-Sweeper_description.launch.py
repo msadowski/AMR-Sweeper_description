c0aaa00a62c63a9e522cd335a8fb856d89dea51c
Alf-Olsen <123420311+Alf-Olsen@users.noreply.github.com>
2024-10-14 13:20:10 +0200

    update

--- launch/AMR-Sweeper_description.launch.py
+++ launch/AMR-Sweeper_description.launch.py
@@ -2,11 +2,15 @@
 import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
+from launch.substitutions import LaunchConfiguration
+from launch.actions import DeclareLaunchArgument
 from launch_ros.actions import Node
 import xacro
 
 
 def generate_launch_description():
+
+    use_sim_time = LaunchConfiguration('use_sim_time')
 
     # The name of the package and path to xacro file within the package
     package_name = 'AMR-Sweeper_description'
@@ -24,11 +28,16 @@ def generate_launch_description():
         package='robot_state_publisher',
         executable='robot_state_publisher',
         output='screen',
-        parameters=[{'robot_description': robot_description_raw}]
+        parameters=[{'robot_description': robot_description_raw, 'use_sim_time': use_sim_time}]
     )
 
 
     # Run the nodes
     return LaunchDescription([
+        DeclareLaunchArgument(
+            'use_sim_time',
+            default_value='false',
+            description='Use sim time if true'),
+            
         node_robot_state_publisher
     ])
