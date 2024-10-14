01a89e0de1690c8d378ecd31d7462744b578e559
Alf-Olsen <123420311+Alf-Olsen@users.noreply.github.com>
2024-10-14 13:04:35 +0200

    update

--- launch/AMR-Sweeper_description.launch.py
+++ launch/AMR-Sweeper_description.launch.py
@@ -24,7 +24,7 @@ def generate_launch_description():
         package='robot_state_publisher',
         executable='robot_state_publisher',
         output='screen',
-        parameters=[{'robot_description': robot_description_raw}]
+        parameters=[{'AMR-Sweeper_description': robot_description_raw}]
     )
 
 
